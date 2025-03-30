11f1e909f53349c9001289259bea67d9fb72489d
José Wynder <wynder.jose@gmail.com>
2025-03-30 13:46:16 -0300

    Review guide, corrections, and missing NoSQL files added

--- src/br/com/cod3r/cm/visao/TelaPrincipal.java
+++ src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -8,12 +8,12 @@ import br.com.cod3r.cm.modelo.Tabuleiro;
 public class TelaPrincipal extends JFrame {
 
 	public TelaPrincipal() {
-		Tabuleiro tabuleiro = new Tabuleiro(10, 18, 30);
+		Tabuleiro tabuleiro = new Tabuleiro(5, 9, 10); // Valor original: (10, 18, 30)
 		
 		add(new PainelTabuleiro(tabuleiro));
 		
 		setTitle("Campo Minado");
-		setSize(690, 438);
+		setSize(517, 328); // Valor original: (690, 438)
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // DISPOSE_ON_CLOSE
 		setVisible(true);
@@ -21,5 +21,16 @@ public class TelaPrincipal extends JFrame {
 	
 	public static void main(String[] args) {
 		new TelaPrincipal();
+		
+		// Precisaria de alguns ajustes para tonar o jogo funcional.
+		// Como, por exemplo:
+		// Fazer com que o primeiro click seja obrigatoriamente em um campo não minado.
+		// Fazer com que o primeiro click seja obrigatoriamente no campo que abra a maior quantidade possível de outros campos.
+		// Fazer com que não exitam situações em que é logicamente impossível acertar o campo. // Existem essas situações, ou seja, lógica do jogo mal otimizada.
+		
+		// Essas pequenas alterações, impossibilitaria que o jogador perca no primeiro click.
+		// Também fazia com que não houvesse situações onde seu primeiro click seja em um campo isolado e cheio de bombas.
+		
+		
 	}
 }
